362fbe67298abf4ba98807d57a539af8fb35c73f
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-23 13:24:49 +0300

    boxing - unboxing

--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -9,3 +9,16 @@ long sayi2 = sayi;
 //explicit
 long sayi3 = 500;
 int sayi4 = (int) sayi3;
+
+
+// value type => byte , int , long , decimal
+int deger = 100;
+
+
+// reference type =>  class , interface , delegate
+
+object deger2 = deger;//boxing 
+
+object deger3 = 100;
+
+int deger4 =(int) deger3; // unboxing 
