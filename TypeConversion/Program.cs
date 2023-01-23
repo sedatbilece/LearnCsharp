9314790c3f0753e3d64b9b3695ba0463f5630909
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-23 14:17:18 +0300

    is and as type check

--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -12,13 +12,35 @@ int sayi4 = (int) sayi3;
 
 
 // value type => byte , int , long , decimal
-int deger = 100;
-
-
 // reference type =>  class , interface , delegate
 
+
+int deger = 100;
+
 object deger2 = deger;//boxing 
+
+
 
 object deger3 = 100;
 
 int deger4 =(int) deger3; // unboxing 
+
+
+
+Object[] dizi = { "fatih", 5, 3.14, false, "sedat", true, 50 };
+
+foreach(var i in dizi)
+{
+    if( i is string) // typeof(i) == string ?? true : false
+    {
+        Console.WriteLine(i);
+
+    }
+}
+Console.WriteLine("********\n");
+
+foreach (var i in dizi)
+{
+    var data = i as string; // typeof(i)==string ?? i : null
+        Console.WriteLine(data);
+}
